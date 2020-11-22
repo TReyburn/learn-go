486cec375ec71e16678fcb62e553facafb366f8a
Travis Reyburn <travisreyburn@gmail.com>
2020-11-21 22:29:17 -0700

    added new func to return a card string

--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
-	card := "Ace of Spades" //More concise
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
