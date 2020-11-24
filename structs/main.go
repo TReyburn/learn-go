b4529d48a72b60c5f3f112d6dfeb9990f3d0dad9
Travis Reyburn <treyburn@alteryx.com>
2020-11-24 15:57:45 -0700

    added struct code

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName string
+	age int
+}
+
 func main() {
-	
+	// don't do this
+	solveig := person{"Solveig", "Delabroye", 28}
+	// do this
+	fmt.Println(solveig)
+	travis := person{
+		firstName: "Travis",
+		lastName:  "Reyburn",
+		age:       30,
+	}
+	fmt.Println(travis)
+
+	var alex person
+	fmt.Printf("%+v", alex)
+
+	alex.firstName = "Alex"
+	alex.lastName = "Smith"
+
+	fmt.Printf("%+v", alex)
 }
