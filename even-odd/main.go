fbcbafd758fa2607d524f6ba638a0aa9c614263b
Travis Reyburn <treyburn@alteryx.com>
2020-11-24 15:10:12 -0700

    filled in code to check if values are even or odd

--- even-odd/main.go
+++ even-odd/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	// Create our int slice from 0 to 10
+	var s []int
+	for i := 0; i <= 10; i++ {
+		s = append(s, i)
+	}
+
+	// Using modulus to check if number is even or odd for each int in our slice
+	for _, n := range s {
+		if n % 2 != 0 {
+			fmt.Println(n, "is odd")
+		} else {
+			fmt.Println(n, "is even")
+		}
+	}
 }
