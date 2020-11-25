2d8ffceb2d662400c58f6c01228dad80d14b4151
Travis Reyburn <travisreyburn@gmail.com>
2020-11-24 20:18:11 -0700

    added some map experiments

--- maps/main.go
+++ maps/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	colors := map[string]string{
+		"red": "#ff000",
+		"green": "#4b781",
+	}
+	delete(colors, "red")
+	fmt.Println(colors)
+
+	var otherColors map[string]string
+	fmt.Println(otherColors)
+
+	anotherColors := make(map[string]string)
+	anotherColors["black"] = "#0000"
+	fmt.Println(anotherColors)
 }
