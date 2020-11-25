7bfb9c73935efaec27826c5f4eb08f6a2c4a15ab
Travis Reyburn <travisreyburn@gmail.com>
2020-11-24 20:56:48 -0700

    added some structs to work off of

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,18 @@
 package main
 
+type englishBot struct {}
+type spanishBot struct {}
+
+func (englishBot) getGreeting() string {
+	// Well just pretend we have some very custom logic here
+	return "Hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	// And we'll keep pretending here
+	return "Hola!"
+}
+
 func main() {
 
 }
