f104a54dc437a2ca7ee2ea332b5d1fb5dc756cc5
Travis Reyburn <treyburn@alteryx.com>
2020-11-30 13:37:45 -0700

    filled in main func

--- shapes/main.go
+++ shapes/main.go
@@ -15,7 +15,25 @@ type shape interface {
 	getArea() float64
 }
 
-func main() {}
+func main() {
+	s1 := square{sideLength: 10.2}
+	printArea(s1)
+
+	s2 := square{sideLength: 2}
+	printArea(s2)
+
+	t1 := triangle{
+		height: 5.3,
+		base:   1.1,
+	}
+	printArea(t1)
+
+	t2 := triangle{
+		height: 3,
+		base:   2,
+	}
+	printArea(t2)
+}
 
 func (t triangle) getArea() float64 {
 	return t.base * t.height * 0.5
@@ -26,5 +44,5 @@ func (s square) getArea() float64 {
 }
 
 func printArea(s shape) {
-	fmt.Printf("Area: %+v", s.getArea())
+	fmt.Printf("Area: %+v \n", s.getArea())
 }
