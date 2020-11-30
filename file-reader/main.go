3111f47fe4003358d74bde1410990445cf230eb5
Travis Reyburn <treyburn@alteryx.com>
2020-11-30 15:08:03 -0700

    added some early exception catching

--- file-reader/main.go
+++ file-reader/main.go
@@ -10,12 +10,20 @@ type fileContent struct {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Need to include a file to be read")
+		os.Exit(1)
+	}
+
 	fn := os.Args[1]
 	fc := fileContent{}
-	fh, err := os.Open(fn); if err != nil {
+	fh, err := os.Open(fn)
+
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	_, err = io.Copy(fc, fh)
 }
 
