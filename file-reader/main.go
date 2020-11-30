1977c5e240a149b4ac8486b2729c979c6d65f962
Travis Reyburn <treyburn@alteryx.com>
2020-11-30 14:54:39 -0700

    filled in func stubs

--- file-reader/main.go
+++ file-reader/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+type fileContent struct {
+}
+
 func main() {
-	argv := os.Args
-	for _, a:= range argv {
-		fmt.Println(a)
+	fn := os.Args[1]
+	fc := fileContent{}
+	fh, err := os.Open(fn); if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	_, err = io.Copy(fc, fh)
+}
+
+func (fc fileContent) Write(b []byte) (n int, err error) {
+	fmt.Println(string(b))
+	return len(b), nil
 }
