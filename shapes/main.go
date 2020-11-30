bcc8a45e730acd91c211b1165097433fd42977c9
Travis Reyburn <treyburn@alteryx.com>
2020-11-30 13:22:00 -0700

    added structs, interfaces, and functions

--- shapes/main.go
+++ shapes/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func main() {}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height * 0.5
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Printf("Area: %+v", s.getArea())
 }
