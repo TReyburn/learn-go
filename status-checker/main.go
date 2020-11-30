2f63892317c6fdf1aea6f7f248f32608348f9cd9
Travis Reyburn <treyburn@alteryx.com>
2020-11-30 16:20:41 -0700

    created our synchronous code

--- status-checker/main.go
+++ status-checker/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+	urls := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"http://totallyfake.abc",
+		"http://alteryx.com",
+		"http://reddit.com",
+		"http://colorado.edu",
+		"http://www.solveigdelabroye.me",
+	}
+
+	for _, u := range urls {
+		checkUrl(u)
+	}
+}
+
+func checkUrl(url string) {
+	_, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Status:", url, "might be down.")
+		return
+	}
+
+	fmt.Println("Status:", url, "is up.")
 }
